e9c86103b949b5aad55c7a4ee54baafe3346f3f6
Edson Domingos <edomingoswork@gmail.com>
2023-03-08 02:18:33 -0300

    update numero ingrid

--- linksIngrid/script.js
+++ linksIngrid/script.js
@@ -4,12 +4,12 @@ whatsapp.forEach((e,f) => {
 		e.addEventListener('click', function() {
 		if (window.screen.width > 452){
 			whatsapp[f].setAttribute(
-				'href','https://web.whatsapp.com/send?phone=5584991661673&text=Olá,+quero+agendar+um+dia!'
+				'href','https://web.whatsapp.com/send?phone=5584997047896&text=Olá,+quero+agendar+um+dia!'
 			)
 			whatsapp[f].setAttribute('target','_blank')
 		} else {
 			whatsapp[f].setAttribute(
-				'href','https://api.whatsapp.com/send?phone=5584991661673&text=Olá,+quero+agendar+um+dia!'
+				'href','https://api.whatsapp.com/send?phone=5584997047896&text=Olá,+quero+agendar+um+dia!'
 			)
 			whatsapp[f].setAttribute('target','_blank')
 		}
