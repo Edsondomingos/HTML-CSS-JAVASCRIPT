6086df67c27c30cc00c260f5a440f07fd9b69fac
Edson Domingos <edomingoswork@gmail.com>
2024-12-29 23:00:05 -0300

    melhorias de aparencia ramo-canguaretama

--- ramo-canguaretama/script.js
+++ ramo-canguaretama/script.js
@@ -18,6 +18,6 @@ const inputs = document.querySelectorAll('input')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    enviar(encodeURI(`${window.location.origin}/convidado.html?convidado=${inputs[0].value}&data=${inputs[1].value}&tema=${inputs[2].value}&tempo=${inputs[3].value}&orador=${inputs[4].value}`))
+    enviar(encodeURI(`${window.location.origin}/ramo-canguaretama/convidado.html?convidado=${inputs[0].value}&data=${inputs[1].value}&tema=${inputs[2].value}&tempo=${inputs[3].value}&orador=${inputs[4].value}`))
     // inputs.forEach(input => console.log(input.value))
 })
